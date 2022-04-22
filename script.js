60eb86ca8f723951b1d8d98f2833f24a52c5f287
Joel Wanjala <joel.n.wanjala@gmail.com>
2022-04-22 10:14:18 +0300

    Implemented the log in functionality

--- script.js
+++ script.js
@@ -12,14 +12,20 @@ const password = document.querySelector('.password');
 const confirmPassword = document.querySelector('.confirm-password');
 const signInForm = document.querySelector('.sign-in__form');
 
+const logIn = document.querySelector('.log-in');
+const logInForm = document.querySelector('.log-in__form');
+const logEmail = document.getElementById('log-email');
+const logPassword = document.getElementById('log-password');
+const logError = document.querySelector('.log-error');
+const logInClose = document.querySelector('.log-in__times');
+
 const errorMsg = document.querySelector('.error-msg');
 
 // Add an event listener to open the sign in modal button
 signInModal.addEventListener('click', function () {
   // Some code
-  signIn.classList.toggle('sign-in__modal');
   signIn.style.display = 'block';
-  console.log('Hello');
+  logIn.style.display = 'none';
 });
 
 // Add an event listener to close the sign in modal button
@@ -38,7 +44,7 @@ signInForm.addEventListener('submit', function (e) {
   if (
     fullName.value === '' &&
     email.value === '' &&
-    Date.value === '' &&
+    date.value === '' &&
     password.value === '' &&
     confirmPassword.value === ''
   ) {
@@ -52,10 +58,53 @@ signInForm.addEventListener('submit', function (e) {
       studentName: fullName.value.trim(),
       studentEmail: email.value.trim(),
       studentBirthYear: date.value,
-      studentPassword: password.value,
+      studentPassword: password.value.trim(),
     };
 
     user.push(studentsList);
-    console.log(user);
+    localStorage.setItem('student', JSON.stringify(user));
+
+    fullName.value =
+      email.value =
+      date.value =
+      password.value =
+      confirmPassword.value =
+        '';
   }
+});
+
+// Add an event listener to the log in button
+logInModal.addEventListener('click', function () {
+  logIn.style.display = 'block';
+  signIn.style.display = 'none';
+});
+
+logInClose.addEventListener('click', function () {
+  logIn.style.display = 'none';
+});
+
+// Log in functionality
+logInForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  // Get the data from the local storage
+  const data = JSON.parse(localStorage.getItem('student'));
+
+  // Get the data from the UI
+  data.forEach(function (student, i) {
+    if (
+      logEmail.value.trim() !== student.studentEmail &&
+      logPassword.value.trim() !== student.studentPassword
+    ) {
+      logError.textContent = 'Invalid credentials';
+      logEmail.classList.add('error-border');
+      logPassword.classList.add('error-border');
+    } else {
+      logEmail.value = logPassword.value = '';
+
+      alert('You have successifully logged in');
+
+      logIn.style.display = 'none';
+    }
+  });
 });
